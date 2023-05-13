90e46c293ceae96f6c69748bb3ec8dfb8404ec2b
shubhu1026 <shubhu.p2000@gmail.com>
2023-05-13 18:58:20 +0530

    added second player and multiple balls

--- SDL Pong/Game.cpp
+++ SDL Pong/Game.cpp
@@ -3,7 +3,15 @@
 Game::Game()
 {
 	mWindow = nullptr;
+	mRenderer = nullptr;
 	mIsRunning = true;
+	mTicksCount = 0;
+	mPaddlePos = { 0,0 };
+	mPaddle2Pos = { 0,0 };
+	mBallPos = { 0,0 };
+	mBallVel = { 0,0 };
+	mPaddleDir = 0;
+	mPaddle2Dir = 0;
 }
 
 bool Game::Initialize()
@@ -20,8 +28,8 @@ bool Game::Initialize()
 		"Game Programming in C++ (Chapter 1)", // Window title
 		100, // Top left x-coordinate of window
 		100, // Top left y-coordinate of window
-		1024, // Width of window
-		768, // Height of window
+		mWindowW, // Width of window
+		mWindowH, // Height of window
 		0 // Flags (0 for no flags set)
 	);
 
@@ -42,6 +50,27 @@ bool Game::Initialize()
 		SDL_Log("Failed to create renderer: %s", SDL_GetError());
 		return false;
 	}
+
+	//Set values for inital gameobject positions;
+	mPaddlePos.x = 40;
+	mPaddlePos.y = mWindowH /2;
+	mPaddle2Pos.x = mWindowW - 40;
+	mPaddle2Pos.y = mWindowH / 2;
+	//mBallPos.x = mWindowW / 2;
+	//mBallPos.y = mWindowH / 2;
+
+	ball1.ballPos.x = mWindowW / 2;
+	ball1.ballPos.y = mWindowH / 2 + 20;
+	ball2.ballPos.x = mWindowW / 2;
+	ball2.ballPos.y = mWindowH / 2 - 20;
+
+	//initial ball velocity
+	ball1.ballVel = { -200.0f, 235.0f };
+	ball2.ballVel = { 200.0f, -235.0f };
+
+	//add balls to vector
+	balls.push_back(ball1);
+	balls.push_back(ball2);
 
 	return true;
 }
@@ -84,10 +113,139 @@ void Game::ProcessInput()
 	{
 		mIsRunning = false;
 	}
+
+	mPaddleDir = 0;
+	if (state[SDL_SCANCODE_W])
+	{
+		mPaddleDir -= 1;
+	}
+	if (state[SDL_SCANCODE_S])
+	{
+		mPaddleDir += 1;
+	}
+
+	mPaddle2Dir = 0;
+	if (state[SDL_SCANCODE_I])
+	{
+		mPaddle2Dir -= 1;
+	}
+	if (state[SDL_SCANCODE_K])
+	{
+		mPaddle2Dir += 1;
+	}
 }
 
 void Game::UpdateGame()
 {
+	//wait until 16ms has elasped since last frame
+	while (!SDL_TICKS_PASSED(SDL_GetTicks(), mTicksCount + 16));
+
+	// Delta time is the difference in ticks from last frame
+	// (converted to seconds)
+	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
+	// Update tick counts (for next frame)
+	mTicksCount = SDL_GetTicks();
+
+	// Clamp maximum delta time value
+	if (deltaTime > 0.05f)
+	{
+		deltaTime = 0.05f;
+	}
+
+	//move paddle
+	if (mPaddleDir != 0)
+	{
+		mPaddlePos.y += mPaddleDir * 300.0f * deltaTime;
+
+		// Make sure paddle doesn't move off screen!
+		if (mPaddlePos.y < (paddleH / 2.0f + thickness))
+		{
+			mPaddlePos.y = paddleH / 2.0f + thickness;
+		}
+		else if (mPaddlePos.y > (mWindowH - paddleH / 2.0f - thickness))
+		{
+			mPaddlePos.y = mWindowH - paddleH / 2.0f - thickness;
+		}
+	}
+
+	//move paddle 2
+	if (mPaddle2Dir != 0)
+	{
+		mPaddle2Pos.y += mPaddle2Dir * 300.0f * deltaTime;
+
+		// Make sure paddle 2 doesn't move off screen!
+		if (mPaddle2Pos.y < (paddleH / 2.0f + thickness))
+		{
+			mPaddle2Pos.y = paddleH / 2.0f + thickness;
+		}
+		else if (mPaddle2Pos.y > (mWindowH - paddleH / 2.0f - thickness))
+		{
+			mPaddle2Pos.y = mWindowH - paddleH / 2.0f - thickness;
+		}
+	}
+
+	//move balls
+	for (Ball& ball : balls)
+	{
+		ball.ballPos.x += ball.ballVel.x * deltaTime;
+		ball.ballPos.y += ball.ballVel.y * deltaTime;
+	}
+
+	for (Ball& ball : balls)
+	{
+		//Collision with top wall
+		if (ball.ballPos.y <= thickness && ball.ballVel.y < 0.0f)
+		{
+			ball.ballVel.y *= -1;
+		}
+		//Collision with bottom wall
+		if (ball.ballPos.y >= mWindowH - thickness && ball.ballVel.y > 0.0f)
+		{
+			ball.ballVel.y *= -1;
+		}
+		//Collision with left wall
+		if (ball.ballPos.x <= 0 && ball.ballVel.x < 0.0f)
+		{
+			ball.ballVel.x *= -1;
+		}
+		//Collision with right wall
+		if (ball.ballPos.x >= mWindowW - thickness && ball.ballVel.x > 0.0f)
+		{
+			ball.ballVel.x *= -1;
+		}
+
+		//Collision with paddles
+		float diff = abs((ball.ballPos.y + thickness / 2) - (mPaddlePos.y + paddleH / 2));
+
+		//Collision with paddle 1
+		if (
+			// Our y-difference is small enough
+			diff <= paddleH / 2.0f &&
+			// Ball is at the correct x-position
+			ball.ballPos.x + thickness/2 <= (mPaddlePos.x + paddleW + 1) && (ball.ballPos.x + thickness / 2 >= mPaddlePos.x) &&
+			// The ball is moving to the left
+			ball.ballVel.x < 0.0f
+			)
+		{
+			ball.ballVel.x *= -1.0f;
+		}
+
+		//Collision with paddle 2
+		float diff2 = abs((ball.ballPos.y + thickness / 2) - (mPaddle2Pos.y + paddleH / 2));
+
+		if (
+			// Our y-difference is small enough
+			diff2 <= paddleH / 2.0f && 
+			// Ball is at the correct x-position
+			ball.ballPos.x + thickness / 2 <= (mPaddle2Pos.x + paddleW) && (ball.ballPos.x + thickness / 2 >= mPaddle2Pos.x - 1) &&
+			// The ball is moving to the Right
+			ball.ballVel.x > 0.0f
+			)
+		{
+			ball.ballVel.x *= -1.0f;
+		}
+
+	}
 }
 
 void Game::GenerateOutput()
@@ -103,6 +261,72 @@ void Game::GenerateOutput()
 	SDL_RenderClear(mRenderer);
 
 	//TODO - Drawing current scene 
+	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
+
+	SDL_Rect topWall {
+		0, // Top left x
+		0, // Top left y
+		mWindowW, // Width
+		thickness // Height
+	};
+
+	SDL_Rect bottomWall{
+		0, // Top left x
+		mWindowH - thickness, // Top left y
+		mWindowW, // Width
+		thickness // Height
+	};
+
+	SDL_Rect leftWall{
+		0, // Top left x
+		0, // Top left y
+		thickness, // Width
+		mWindowH // Height
+	};
+
+	SDL_Rect rightWall{
+		mWindowW - thickness, // Top left x
+		0, // Top left y
+		thickness, // Width
+		mWindowH // Height
+	};
+
+	SDL_Rect firstBall{
+		static_cast<int>(balls[0].ballPos.x - thickness / 2),
+		static_cast<int>(balls[0].ballPos.y - thickness / 2),
+		thickness,
+		thickness
+	};
+
+	SDL_Rect secondBall{
+		static_cast<int>(balls[1].ballPos.x - thickness / 2),
+		static_cast<int>(balls[1].ballPos.y - thickness / 2),
+		thickness,
+		thickness
+	};
+
+	SDL_Rect paddle{
+		static_cast<int>(mPaddlePos.x - paddleW / 2),
+		static_cast<int>(mPaddlePos.y - paddleH / 2),
+		paddleW,
+		paddleH
+	};
+
+	SDL_Rect paddle2{
+		static_cast<int>(mPaddle2Pos.x - paddleW / 2),
+		static_cast<int>(mPaddle2Pos.y - paddleH / 2),
+		paddleW,
+		paddleH
+	};
+
+	SDL_RenderFillRect(mRenderer, &topWall);
+	SDL_RenderFillRect(mRenderer, &bottomWall);
+	SDL_RenderFillRect(mRenderer, &leftWall);
+	SDL_RenderFillRect(mRenderer, &rightWall);
+	SDL_RenderFillRect(mRenderer, &firstBall);
+	SDL_RenderFillRect(mRenderer, &secondBall);
+	SDL_RenderFillRect(mRenderer, &paddle);
+	SDL_RenderFillRect(mRenderer, &paddle2);
 
 	SDL_RenderPresent(mRenderer);
 }
--- SDL Pong/Game.h
+++ SDL Pong/Game.h
@@ -1,5 +1,18 @@
 #pragma once
 #include<SDL2/SDL.h>
+#include<vector>
+
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+struct Ball
+{
+	Vector2 ballPos;
+	Vector2 ballVel;
+};
 
 class Game
 {
@@ -16,9 +29,30 @@ private:
 	void ProcessInput();
 	void UpdateGame();
 	void GenerateOutput();
+
 	// Window created by SDL
 	SDL_Window* mWindow;
 	SDL_Renderer* mRenderer;
+
+	//Window Variables
+	const int mWindowH = 768;
+	const int mWindowW = 1024;
+
+	//Delta Time
+	Uint32 mTicksCount;
+
+	//Game Logic
+	int mPaddleDir;
+	int mPaddle2Dir;
+	std::vector<Ball> balls;
+
+	//Game objects
+	Vector2 mPaddlePos, mPaddle2Pos, mBallPos, mBallVel;
+	Ball ball1, ball2;
+	const int paddleH = 100;
+	const int paddleW = 15;
+	const int thickness = 15;
+
 	// Game should continue to run
 	bool mIsRunning;
 };
